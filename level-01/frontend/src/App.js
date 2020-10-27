6002665d30c404ba51f8ffd76dbc6d300fa2ad2a
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-10-27 14:13:29 -0400

    useState and Imutability

--- level-01/frontend/src/App.js
+++ level-01/frontend/src/App.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from './components/Header'
 
 function App() {
+  const [projects, setProjects] = useState(['Deselvolvimento de app', 'Front-end web'])
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`])
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Home</li>
-          <li>Dashboard</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>SignIn</li>
-        </ul>
-      </Header>
+      <Header title="Homepage" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   )
 }
