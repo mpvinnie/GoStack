d17d45212fdd8d3aacdfd65375d076af5cca80ca
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-02 15:34:50 -0400

    separing on repositories

---
+++ level-02/server/src/controllers/AppoitmentsController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express'
+import { startOfHour, parseISO } from 'date-fns'
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+
+const appointmentsRepository = new AppointmentsRepository()
+
+export default class AppointmentsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { provider, date } = request.body
+
+    const parsedDate = startOfHour(parseISO(date))
+
+    const findAppointment = appointmentsRepository.findByDate(date)
+
+    if (findAppointment) {
+      return response
+        .status(400)
+        .json({ error: 'This appointment is already booked' })
+    }
+
+    const appointment = appointmentsRepository.create(provider, parsedDate)
+
+    return response.json(appointment)
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const appointments = appointmentsRepository.findAll()
+    return response.json(appointments)
+  }
+}
--- level-02/server/src/routes/appointments.routes.ts
+++ level-02/server/src/routes/appointments.routes.ts
@@ -1,41 +1,12 @@
 import { Router } from 'express'
-import { v4 as uuid } from 'uuid'
-import { startOfHour, parseISO, isEqual } from 'date-fns'
+
+import AppointmentsController from '../controllers/AppoitmentsController'
 
 const appointmentsRouter = Router()
 
-interface IAppointment {
-  id: string
-  provider: string
-  date: Date
-}
+const appointmentsController = new AppointmentsController()
 
-const appointments: IAppointment[] = []
-
-appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body
-
-  const parsedDate = startOfHour(parseISO(date))
-
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date)
-  )
-
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ error: 'This appointment is already booked' })
-  }
-
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate
-  }
-
-  appointments.push(appointment)
-
-  return response.json(appointments)
-})
+appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.get('/', appointmentsController.index)
 
 export default appointmentsRouter
