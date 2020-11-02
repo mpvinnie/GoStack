dca9e9ca4a089177970c5c91521668ae6410217e
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-02 15:01:16 -0400

    create appointment

--- level-02/server/src/server.ts
+++ level-02/server/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 
+import routes from './routes'
+
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world 3' })
-})
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!')
