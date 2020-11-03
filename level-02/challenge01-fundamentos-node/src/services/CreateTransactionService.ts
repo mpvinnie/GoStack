22032c7270229b1da3565651acbc8329a760895a
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-03 03:16:16 -0400

    finish challenge node

---
+++ level-02/challenge01-fundamentos-node/src/services/CreateTransactionService.ts
@@ -0,0 +1,36 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error(
+        'You cannot create a outcome transaction larger than total balance ',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
