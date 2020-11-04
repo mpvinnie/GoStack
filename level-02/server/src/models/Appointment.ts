e8944d0c912e0839dc2fb91f3cb2202f948fbd03
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-04 06:40:45 -0400

    relations

--- level-02/server/src/models/Appointment.ts
+++ level-02/server/src/models/Appointment.ts
@@ -2,8 +2,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn
 } from 'typeorm'
+
+import User from './User'
 
 @Entity('appointments')
 class Appointment {
@@ -11,7 +15,11 @@ class Appointment {
   id: string
 
   @Column()
-  provider: string
+  provider_id: string
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User
 
   @Column('timestamp with time zone')
   date: Date
