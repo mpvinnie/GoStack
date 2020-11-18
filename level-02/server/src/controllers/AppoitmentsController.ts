ee568213b2d50fd6013aea04e690ff58278a7d67
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-18 14:47:39 -0400

    Global exception handler

--- level-02/server/src/controllers/AppoitmentsController.ts
+++ level-02/server/src/controllers/AppoitmentsController.ts
@@ -15,21 +15,17 @@ export default class AppointmentsController {
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    try {
-      const { provider_id, date } = request.body
+    const { provider_id, date } = request.body
 
-      const parsedDate = parseISO(date)
+    const parsedDate = parseISO(date)
 
-      const createAppointment = new CreateAppointmentService()
+    const createAppointment = new CreateAppointmentService()
 
-      const appointment = await createAppointment.execute({
-        date: parsedDate,
-        provider_id
-      })
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id
+    })
 
-      return response.json(appointment)
-    } catch (err) {
-      return response.status(400).json({ error: err.message })
-    }
+    return response.json(appointment)
   }
 }
--- level-02/server/src/controllers/SessionsController.ts
+++ level-02/server/src/controllers/SessionsController.ts
@@ -4,20 +4,16 @@ import AuthenticateUserService from '../services/AuthenticateUserService'
 
 class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    try {
-      const { email, password } = request.body
+    const { email, password } = request.body
 
-      const authenticateUser = new AuthenticateUserService()
+    const authenticateUser = new AuthenticateUserService()
 
-      const { user, token } = await authenticateUser.execute({
-        email,
-        password
-      })
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password
+    })
 
-      return response.json({ user, token })
-    } catch (err) {
-      return response.status(401).json({ error: err.message })
-    }
+    return response.json({ user, token })
   }
 }
 
