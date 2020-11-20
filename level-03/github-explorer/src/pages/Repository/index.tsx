9643f3467d11ae86306eb60079194b187014efb0
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-19 21:42:38 -0400

    navigating routes

--- level-03/github-explorer/src/pages/Repository/index.tsx
+++ level-03/github-explorer/src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useRouteMatch } from 'react-router-dom'
+
+interface IRepositoryParams {
+  repository: string
+}
 
 const Repository: React.FC = () => {
-  return <h1> Repository</h1>
+  const { params } = useRouteMatch<IRepositoryParams>()
+
+  return <h1> Repository: {params.repository}</h1>
 }
 
 export default Repository
