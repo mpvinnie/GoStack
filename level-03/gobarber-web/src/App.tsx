605895b7e4f402a89e80acd9bab16bf3a5072645
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-20 13:55:11 -0400

    SignIn Page

--- level-03/gobarber-web/src/App.tsx
+++ level-03/gobarber-web/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import GlobalStyle from './styles/global'
 
+import SignIn from './pages/SignIn'
+
 const App: React.FC = () => {
   return (
     <>
-      <h1>GoBarber</h1>
+      <SignIn />
       <GlobalStyle />
     </>
   )
