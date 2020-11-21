66fd583fd623f209823e29bc67d806281eefe449
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-21 16:47:01 -0400

    validanting datas

--- level-03/gobarber-web/src/pages/SignUp/index.tsx
+++ level-03/gobarber-web/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react'
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi'
 import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -10,8 +11,22 @@ import Button from '../../components/Button'
 import { Container, Background, Content } from './styles'
 
 const SignUp: React.FC = () => {
-  const handleSubmit = useCallback((data: Object) => {
-    console.log(data)
+  const handleSubmit = useCallback(async (data: Object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos')
+      })
+
+      await schema.validate(data, {
+        abortEarly: false
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }, [])
 
   return (
