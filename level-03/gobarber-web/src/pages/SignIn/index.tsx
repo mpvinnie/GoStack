91b8b4e544e1859828cf54dae1a7d8579c610d58
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-22 00:39:52 -0400

    creating toast hook

--- level-03/gobarber-web/src/pages/SignIn/index.tsx
+++ level-03/gobarber-web/src/pages/SignIn/index.tsx
@@ -4,7 +4,8 @@ import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
 
-import { useAuth } from '../../hooks/AuthContext'
+import { useAuth } from '../../hooks/auth'
+import { useToast } from '../../hooks/toast'
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.svg'
@@ -23,6 +24,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
   const { signIn } = useAuth()
+  const { addToast } = useToast()
 
   const handleSubmit = useCallback(
     async (data: ISignInFormData) => {
@@ -40,16 +42,20 @@ const SignIn: React.FC = () => {
           abortEarly: false
         })
 
-        signIn({ email: data.email, password: data.password })
+        await signIn({ email: data.email, password: data.password })
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err)
 
           formRef.current?.setErrors(errors)
+
+          return
         }
+
+        addToast()
       }
     },
-    [signIn]
+    [signIn, addToast]
   )
 
   return (
