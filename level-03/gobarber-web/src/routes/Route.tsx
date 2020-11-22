42182ef9586690917846cb9dd598ff922185bd06
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-22 11:50:58 -0400

    private routes

---
+++ level-03/gobarber-web/src/routes/Route.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable */
+import React from 'react'
+import { RouteProps, Route as ReactDomRoute, Redirect } from 'react-router-dom'
+
+import { useAuth } from '../hooks/auth'
+
+interface IRouteProps extends RouteProps {
+  isPrivate?: boolean
+  component: React.ComponentType
+}
+
+const Route: React.FC<IRouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth()
+
+  return (
+    <ReactDomRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+            <Redirect
+              to={{
+                pathname: isPrivate ? '/' : '/dashboard',
+                state: { from: location }
+              }}
+            />
+          )
+      }}
+    />
+  )
+}
+
+export default Route
