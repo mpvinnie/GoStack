b76db8bfbc5c49dc5e347bc586fb9e7e22d56567
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-23 13:21:50 -0400

    signup page

--- level-03/appgobarber/src/pages/SignUp/index.tsx
+++ level-03/appgobarber/src/pages/SignUp/index.tsx
@@ -1,14 +1,73 @@
 import React from 'react'
+import {
+  View,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
 
-import { Text } from 'react-native'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
 
-import { Container } from './styles'
+import logoImg from '../../assets/logo.png'
+
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInButtonText
+} from './styles'
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation()
+
   return (
-    <Container>
-      <Text>SignUp</Text>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('deu')
+              }}
+            >
+              Cadastrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignInButton
+        onPress={() => {
+          navigation.goBack()
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInButtonText>Voltar para logon</BackToSignInButtonText>
+      </BackToSignInButton>
+    </>
   )
 }
 
