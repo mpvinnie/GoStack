632069b1785f2eb3ce2bf92ad77d44d78d1d33c8
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-24 12:54:14 -0400

    validating forms

--- level-03/appgobarber/src/components/Input/index.tsx
+++ level-03/appgobarber/src/components/Input/index.tsx
@@ -70,7 +70,7 @@ const Input: React.ForwardRefRenderFunction<IInputRef, IInputProps> = (
   }, [fieldName, registerField])
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={20}
