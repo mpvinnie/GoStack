45c711f0ae8a0b50300f0abd01a6df7c89075da4
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-29 16:59:14 -0400

    send email in dev ambient

--- level-04/server/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ level-04/server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -18,7 +18,7 @@ class SendForgotPasswordEmailService {
     @inject('MailProvider')
     private mailProvider: IMailProvider,
 
-    @inject('UserTokensRepositry')
+    @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository
   ) {
     /* Empty */
@@ -31,9 +31,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists')
     }
 
-    await this.userTokensRepository.generate(user.id)
+    const { token } = await this.userTokensRepository.generate(user.id)
 
-    this.mailProvider.sendMail(email, 'Pedido de email de recuperação recebido')
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de email de recuperação recebido ${token}`
+    )
   }
 }
 
