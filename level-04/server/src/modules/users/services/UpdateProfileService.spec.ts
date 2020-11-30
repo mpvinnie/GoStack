b8b9c4f81375232f17850d90e8b0aa06f0caceea
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-29 23:34:50 -0400

    Update and show profile controllers and routes

--- level-04/server/src/modules/users/services/UpdateProfileService.spec.ts
+++ level-04/server/src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfile', () => {
     expect(updatedUser.email).toBe('johntre@example.com')
   })
 
+  it('should be able to update the user profile from a non-existing user', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'John Trê',
+        email: 'johntre@example.com'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
   it('should not be able to update the user email to a existing one', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
