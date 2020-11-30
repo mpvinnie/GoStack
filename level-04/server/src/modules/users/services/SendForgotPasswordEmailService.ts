f59a3c9e4d2a61b0e646a0ec597f36d52e73bc24
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-29 22:13:11 -0400

    email template handlebars

--- level-04/server/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ level-04/server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -33,10 +33,20 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id)
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de email de recuperação recebido ${token}`
-    )
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token
+        }
+      }
+    })
   }
 }
 
