e97428cc746200a550b03aaa5cc8b6c0e5da44a2
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-30 04:53:44 -0400

    routes and controllers to create an appointment

--- level-04/server/src/modules/appointments/services/CreateAppointmentService.ts
+++ level-04/server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns'
+import { startOfHour, isBefore, getHours } from 'date-fns'
 import { inject, injectable } from 'tsyringe'
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
@@ -27,6 +27,18 @@ class CreateAppointmentService {
     date
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError('You cannot create an appointment on a past date')
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError('You cannot create an appointment with yourself')
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError('You can only create appointments between 8am and 5pm')
+    }
 
     const findAppointment = await this.appointmentsRepository.findByDate({
       date: appointmentDate,
