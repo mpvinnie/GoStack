976558c1e3f517095e4ac73348e2bde34451720c
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-30 23:28:04 -0400

    Validating inputs

--- level-04/server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ level-04/server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import AppointmentsController from '../controllers/AppointmentsController'
 import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController'
@@ -12,7 +13,17 @@ appointmentsRouter.use(ensureAuthenticated)
 const appointmentsController = new AppointmentsController()
 const providerAppointmentsController = new ProviderAppointmentsController()
 
-appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().required(),
+      date: Joi.date()
+    }
+  }),
+  appointmentsController.create
+)
+
 appointmentsRouter.get('/me', providerAppointmentsController.index)
 
 export default appointmentsRouter
